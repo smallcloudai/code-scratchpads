0345c42beaa45f4145903c81ff23895fd86e78da
reymondzzzz <starkov.kirill123@gmail.com>
2023-08-22 16:30:00 +0300

    add dockerfile

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name="code-scratchpads",
-    py_modules=["code_scratchpads"],
+    packages=["code_scratchpads", "code_scratchpads.scratchpads_code_completion"],
     version="0.0.1",
     install_requires=[
         "aiohttp",
         "uvicorn",
         "fastapi",
+        "termcolor",
+        "transformers"
     ],
 )
