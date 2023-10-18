7f2c8549127ac3eeb28d3704c3f7886114c30cbf
Valeryi <ponyparser@gmail.com>
2023-10-18 17:06:58 +0100

    fixed "" in url field

--- src/forward_to_openai_endpoint.rs
+++ src/forward_to_openai_endpoint.rs
@@ -8,16 +8,13 @@ use crate::call_validation::SamplingParameters;
 
 
 pub async fn forward_to_openai_style_endpoint(
-    mut save_url: &String,
+    save_url: &String,
     bearer: String,
     model_name: &str,
     prompt: &str,
     client: &reqwest::Client,
-    endpoint_template: &String,
     sampling_parameters: &SamplingParameters,
 ) -> Result<serde_json::Value, String> {
-    let url = endpoint_template.replace("$MODEL", model_name);
-    save_url.clone_from(&&url);
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json").unwrap());
     if !bearer.is_empty() {
@@ -31,7 +28,7 @@ pub async fn forward_to_openai_style_endpoint(
         "temperature": sampling_parameters.temperature,
         "max_tokens": sampling_parameters.max_new_tokens,
     });
-    let req = client.post(&url)
+    let req = client.post(save_url)
        .headers(headers)
        .body(data.to_string())
        .send()
@@ -39,26 +36,23 @@ pub async fn forward_to_openai_style_endpoint(
     let resp = req.map_err(|e| format!("{}", e))?;
     let status_code = resp.status().as_u16();
     let response_txt = resp.text().await.map_err(|e|
-        format!("reading from socket {}: {}", url, e)
+        format!("reading from socket {}: {}", save_url, e)
     )?;
     // info!("forward_to_openai_style_endpoint: {} {}\n{}", url, status_code, response_txt);
     if status_code != 200 {
-        return Err(format!("{} status={} text {}", url, status_code, response_txt));
+        return Err(format!("{} status={} text {}", save_url, status_code, response_txt));
     }
     Ok(serde_json::from_str(&response_txt).unwrap())
 }
 
 pub async fn forward_to_openai_style_endpoint_streaming(
-    mut save_url: &String,
+    save_url: &String,
     bearer: String,
     model_name: &str,
     prompt: &str,
     client: &reqwest::Client,
-    endpoint_template: &String,
     sampling_parameters: &SamplingParameters,
 ) -> Result<EventSource, String> {
-    let url = endpoint_template.replace("$MODEL", model_name);
-    save_url.clone_from(&&url);
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json").unwrap());
     if !bearer.is_empty() {
@@ -72,11 +66,11 @@ pub async fn forward_to_openai_style_endpoint_streaming(
         "temperature": sampling_parameters.temperature,
         "max_tokens": sampling_parameters.max_new_tokens,
     });
-    let builder = client.post(&url)
+    let builder = client.post(save_url)
        .headers(headers)
        .body(data.to_string());
     let event_source: EventSource = EventSource::new(builder).map_err(|e|
-        format!("can't stream from {}: {}", url, e)
+        format!("can't stream from {}: {}", save_url, e)
     )?;
     Ok(event_source)
 }
